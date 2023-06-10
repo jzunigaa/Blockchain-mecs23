d30310d88c796151dff310b1dc8a14d415d0abfb
jzunigaa <jose.zuniga1@unmsm.edu.pe>
2023-06-09 23:54:28 -0500

    Depurado_cpps

--- SHA256.h
+++ SHA256.h
@@ -2,7 +2,13 @@
 #define SHA256_H
 
 #include <string>
+#include "picosha2.h"
+#include <sstream>
 
-std::string sha256(const std::string& input);
+std::string sha256(const std::string& input) {
+    std::string hash;
+    picosha2::hash256_hex_string(input, hash);
+    return hash;
+}
 
 #endif  // SHA256_H
